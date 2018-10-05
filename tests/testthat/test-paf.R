77d266c4e8d756ccbd4540977591eec2e71aa7f8
Tommi Pajala <tjpajala@gmail.com>
2018-10-05 16:43:20 +0300

    add test coverage

--- tests/testthat/test-paf.R
+++ tests/testthat/test-paf.R
@@ -1,16 +1,20 @@
 context("test-paf")
+dataset_name <- "yle_2011"
+data <- get_dataset(dataset_name)
+q_col=get_data_cols(dataset_name, data)
+party_col <- get_functional_column_name(data,alternative_spellings = c("puolue","Puolue","party"))
+data <- prepare_data(data, q_col, party_col)
+qdata <- dplyr::select(data, dplyr::one_of(party_col, q_col))
 
 test_that("PAF works", {
-  dataset_name <- "yle_2011"
-  data <- get_dataset(dataset_name)
-  q_col=get_data_cols(dataset_name, data)
-  party_col <- get_functional_column_name(data,alternative_spellings = c("puolue","Puolue","party"))
-  data <- prepare_data(data, q_col, party_col)
-  
-  qdata <- dplyr::select(data, dplyr::one_of(party_col, q_col))
   fa <- PAF(qdata, nfactors = 2, vss=FALSE, cols=q_col)
   
   expect_equivalent(dplyr::select(fa$scores,c(-PA1,-PA2)), qdata)
   expect_equal(colnames(fa$loadings),c("PA1","PA2"))
   expect_equal(fa$det, 0.0002640543, tolerance=0.00001)
+})
+
+test_that("VSS can be printed", {
+  fa <- PAF(qdata, nfactors = 2, vss=TRUE, cols=q_col)
+  
 })
